f1ada9fd19e8fc27ed69b67354af8e93d121d77c
Emil Stojanovic <emil@horisen.com>
2019-04-08 17:02:07 +0200

    first animation

--- js/gsap.js
+++ js/gsap.js
@@ -1,5 +1,20 @@
-var $logo = $('.logo');
-$logo.on('click', function(e){
- TweenMax.to($(this), 0.3, {x:'+=100px'});
-});
-//TweenMax.to(".logo", 1.5, {scale:1, xPercent:45});
+var tlGeneral = new TimelineMax({ repeat: -1, repeatDelay: 1 });
+// 1st Btn
+tlGeneral.to(".btn.no1", .2, { 
+    opacity: 9, 
+    scaleX: 1.2, 
+    scaleY: 1.2, 
+    ease: Bounce.easeOut,
+    transformOrigin: "50% 50%" 
+},0 )
+.to(".btn.no1", .5, { opacity: 0, }, .2);
+
+// 2nd Btn
+tlGeneral.to(".btn.no5", 1, { 
+    opacity: 9, 
+    scaleX: 1.2, 
+    scaleY: 1.2, 
+    ease: Bounce.easeOut, 
+    transformOrigin: "50% 50%" 
+},0 )
+.to(".btn.no5", .5, { opacity: 0, }, 1);
